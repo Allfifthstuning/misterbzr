82c617e7d11653a4aa55dbf9006265670488587d
misterbzr <hbezemer@kliksafe.nl>
2020-09-22 15:43:55 +0200

    Added links and set colors right

--- dwm/config.h
+++ dwm/config.h
@@ -58,15 +58,17 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_green, "-nf", col_green, "-sb", col_black, "-sf", col_black, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_black, "-nf", col_green, "-sb", col_green, "-sf", col_black, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *browsercmd[] = { "qutebrowser", NULL };
+static const char *linkscmd[] = { "st", "-e", "links", "www.duckduckgo.com/lite" };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,		        XK_w,      spawn,          {.v = browsercmd } },
+	{ MODKEY|ShiftMask,	        XK_w,      spawn,          {.v = linkscmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
